a1edb0e904b70c7f008dadcb8e796ac4c76793d3
8bei <babei@tuta.io>
2022-08-21 03:39:14 -0400

    【最终方案】使用after伪类解决高度塌陷

---
+++ src/float/07_【最终方案】使用after伪类解决高度塌陷/index.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import "./index.css";
+
+export default function Demo() {
+  return (
+    <Fragment>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </Fragment>
+  );
+}
